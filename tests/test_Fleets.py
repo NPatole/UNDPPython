c5c9189a989a6dab18f0fa5e76f81c9900a56064
Nilesh <nileshpatole11@gmail.com>
2020-06-09 14:46:34 +0530

    Added test cases for Users and Fleets

--- tests/test_Fleets.py
+++ tests/test_Fleets.py
@@ -9,24 +9,26 @@ class TestFleets(BaseClass):
 
     def test_navigateToFleetMenu(self):
 #        time.sleep(5)
+        assert ("testing" == "testing")
+        if not TestFleets.fleets.element_is_visible(TestFleets.fleets.configActiveMenu):
+            TestFleets.fleets.click_on_button(TestFleets.fleets.tenantDropDown)
+            TestFleets.fleets.select_dropdown_text(TestFleets.fleets.teanantList, self.configfileParser("Tenant"))
+            TestFleets.fleets.click_on_button(TestFleets.fleets.configMenu)
         assert ("test" == "testing")
-#         if not TestFleets.fleets.element_is_visible(TestFleets.fleets.configActiveMenu):
-#             TestFleets.fleets.click_on_button(TestFleets.fleets.tenantDropDown)
-#             TestFleets.fleets.select_dropdown_text(TestFleets.fleets.teanantList, self.configfileParser("Tenant"))
-#             TestFleets.fleets.click_on_button(TestFleets.fleets.configMenu)
 
-    # def test_addFleet(self):
-    #     TestFleets.fleets.click_on_button(TestFleets.fleets.fleetsMenu)
-    #     if TestFleets.fleets.entry_present_intable(TestFleets.fleets.fleetTableId, self.configfileParser("FleetName")) == True :
-    #         TestFleets.fleets.deleteFleet()
-    #     TestFleets.fleets.addFleet()
-    #     assert TestFleets.fleets.entry_present_intable(TestFleets.fleets.fleetTableId, self.configfileParser("FleetName")) == True
-    #
-    # def test_deleteFleet(self):
-    #     TestFleets.fleets.click_on_button(TestFleets.fleets.fleetsMenu)
-    #     if TestFleets.fleets.entry_present_intable(TestFleets.fleets.fleetTableId, self.configfileParser("FleetName")) == False :
-    #         TestFleets.fleets.addFleet()
-    #     TestFleets.fleets.deleteFleet()
-    #     assert (TestFleets.fleets.get_text(TestFleets.fleets.alertDialog) == "Baner has been deleted successfully.")
+
+    def test_addFleet(self):
+        TestFleets.fleets.click_on_button(TestFleets.fleets.fleetsMenu)
+        if TestFleets.fleets.entry_present_intable(TestFleets.fleets.fleetTableId, self.configfileParser("FleetName")) == True :
+            TestFleets.fleets.deleteFleet()
+        TestFleets.fleets.addFleet()
+        assert TestFleets.fleets.entry_present_intable(TestFleets.fleets.fleetTableId, self.configfileParser("FleetName")) == True
+
+    def test_deleteFleet(self):
+        TestFleets.fleets.click_on_button(TestFleets.fleets.fleetsMenu)
+        if TestFleets.fleets.entry_present_intable(TestFleets.fleets.fleetTableId, self.configfileParser("FleetName")) == False :
+            TestFleets.fleets.addFleet()
+        TestFleets.fleets.deleteFleet()
+        assert (TestFleets.fleets.get_text(TestFleets.fleets.alertDialog) == "Baner has been deleted successfully.")
 
 
